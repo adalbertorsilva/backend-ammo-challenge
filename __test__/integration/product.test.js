a305fd595d688f5a9c47ba7b71c606a73bc37e7a
adalbertorsilva <adalbertorsilvajr@gmail.com>
2018-10-01 23:24:06 -0300

    - added a default limit and offset to products controller for query reasons

--- __test__/integration/product.test.js
+++ __test__/integration/product.test.js
@@ -3,10 +3,11 @@ const app = require('../../config/app')
 
 describe('PRODUCTS CONTROLLER', () => {
   describe('GET', () => {
-    it('should return a 200 status when getting / from products api', async () => {
+    it('should return a 200 status and a list of 10 objects when getting / from products api', async () => {
       const response = await request(app).get('/api/products')
       expect(response.status).toEqual(200)
       expect(response.body).toHaveProperty('products')
+      expect(response.body.products).toHaveLength(10)
     })
 
     it('should return a 404 status  and an error message when getting any other route from the api', async () => {
--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,14 @@
+const autoBind = require('auto-bind')
 const { Product } = require('../models')
 
 class ProductController {
+  constructor () {
+    autoBind(this)
+    this.limit = 10
+    this.offset = 0
+  }
   async find (req, res) {
-    const products = await Product.findAll()
+    const products = await Product.findAll({ limit: this.limit, offset: this.offset })
     res.status(200).send({ products })
   }
 }

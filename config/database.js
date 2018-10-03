b59792c54b40a568ea4317363fea8643d73047c9
adalbertorsilva <adalbertorsilvajr@gmail.com>
2018-10-03 08:33:01 -0300

    - database config adjustments

--- config/database.js
+++ config/database.js
@@ -19,7 +19,7 @@ module.exports = {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
+    host: process.env.DATABASE_URL,
     dialect: 'postgres'
   }
 }
